e99b0640cc9280299c91f3297480f189e98bbc50
Stanimir Angelov <62446471+Angelov1912@users.noreply.github.com>
2022-01-20 21:53:56 +0000

    Update script.js
    *Add function with event listener that closes the info box
    *Add comments

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+//Show and hide info box on scroll
 $(window).scroll(function () {
-  if ($(this).scrollTop() < 10) {
-    $("#box").hide(200);
+  if ($(this).scrollTop() < 300) {
+    $("#box").hide(300); //Fade in speed when the box opens
   } else {
-    $("#box").show(200);
+    $("#box").show(300); //Fade out speed when the box closes
   }
 });
+
+//Add a function that closes the info box
+//when user clicks on X
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
